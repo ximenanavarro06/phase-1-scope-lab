0618934e7b11cea44d68a1d46951eb5729f35eed
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-04 10:35:33 -0600

    commit tree c544b80945f830e26947a572902db570b68bb41b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = `bob`
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(name) {
+     bestCustomer = `not bob`
+} 
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`
+}
+const leastFavoriteCustomer = `bob for sure`
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Ximena'
+}
